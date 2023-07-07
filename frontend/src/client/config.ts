8076b72a9f405da0ab2068189188bbe3649727bb
Nikita Dilman <dalmannikita@gmail.com>
2023-07-06 22:22:19 -0300

    Update config.ts

--- frontend/src/client/config.ts
+++ frontend/src/client/config.ts
@@ -4,7 +4,7 @@ import {
     testnetConnection
 } from '@mysten/sui.js'
 
-export const CONTRACT_ADDRESS = '0x7fb0e470fc784fbaa6e806d0b4d9781ab0a5a19dc363c8ae484073e520852aba';
+export const CONTRACT_ADDRESS = '0x20d2f7cc55a5abd3e9702f69e502e8bff095a6c65a9b1ff186f9cadf7c3e5577';
 export const PROFILE_ADDR = () => localStorage.getItem('profileAddr');
 export const REGISTRY_ADDRESS = () => localStorage.getItem('registryAddr');
 export const FILECOIN_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDkzMTZCRGUwODhlQzZDMERDODZFNjIxNWJBYzZlNGE2N2ViOWVlNEMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjUyMjM1MjU3MjUsIm5hbWUiOiJ0b2tlbiJ9.y1MPtXn6evn_ozC3jqU68f50RQSb3OMPDQHD-JodubI'
